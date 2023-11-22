f3a2442c71771f95448a6a8a92bbc99050af8d88
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2023-11-22 17:12:47 +0100

    Added class with method main

---
+++ src/main/java/pl/kowalczyk/maciej/java/app/bookingapp/model/Application.java
@@ -0,0 +1,9 @@
+package pl.kowalczyk.maciej.java.app.bookingapp.model;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Property konwalioweZacisze = new Property();
+        konwalioweZacisze.rent();
+    }
+}
