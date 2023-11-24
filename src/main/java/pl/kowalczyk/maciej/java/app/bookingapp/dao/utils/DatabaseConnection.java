ba50ad3cb4b760eb7455ff12ef9ecf937ed53f0f
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2023-11-24 17:21:28 +0100

    Added initiate fields, constructor and getInstance() method

--- src/main/java/pl/kowalczyk/maciej/java/app/bookingapp/dao/utils/DatabaseConnection.java
+++ src/main/java/pl/kowalczyk/maciej/java/app/bookingapp/dao/utils/DatabaseConnection.java
@@ -1,4 +1,21 @@
 package pl.kowalczyk.maciej.java.app.bookingapp.dao.utils;
 
 public class DatabaseConnection {
+
+    private static DatabaseConnection instance;
+    private String url = "jdbc:h2:~/test";
+    private String user = "sa";
+    private String password = "";
+
+    private DatabaseConnection() {
+
+    }
+
+    public static synchronized DatabaseConnection getInstance() {
+        if (instance == null) {
+            instance = new DatabaseConnection();
+        }
+
+        return instance;
+    }
 }
