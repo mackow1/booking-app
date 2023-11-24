d4ada4ff66747684796987b9a8e4217189f5e1f8
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2023-11-24 19:12:59 +0100

    Added method getConnection

--- src/main/java/pl/kowalczyk/maciej/java/app/bookingapp/dao/utils/DatabaseConnection.java
+++ src/main/java/pl/kowalczyk/maciej/java/app/bookingapp/dao/utils/DatabaseConnection.java
@@ -1,11 +1,15 @@
 package pl.kowalczyk.maciej.java.app.bookingapp.dao.utils;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
 
     private static DatabaseConnection instance;
-    private String url = "jdbc:h2:~/test";
-    private String user = "sa";
-    private String password = "";
+    public String url = "jdbc:h2:~/test";
+    public String user = "sa";
+    public String password = "";
 
     private DatabaseConnection() {
 
@@ -17,5 +21,9 @@ public class DatabaseConnection {
         }
 
         return instance;
+    }
+
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, user, password);
     }
 }
