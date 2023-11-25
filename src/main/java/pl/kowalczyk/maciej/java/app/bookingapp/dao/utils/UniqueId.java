86b91eece4f9d6c8b78e97e89c4f63e2f2e8927e
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2023-11-25 22:42:23 +0100

    Added method generate()

--- src/main/java/pl/kowalczyk/maciej/java/app/bookingapp/dao/utils/UniqueId.java
+++ src/main/java/pl/kowalczyk/maciej/java/app/bookingapp/dao/utils/UniqueId.java
@@ -1,4 +1,10 @@
 package pl.kowalczyk.maciej.java.app.bookingapp.dao.utils;
 
+import java.util.Random;
+
 public class UniqueId {
+
+    public static Long generate() {
+        return new Random().nextLong();
+    }
 }
