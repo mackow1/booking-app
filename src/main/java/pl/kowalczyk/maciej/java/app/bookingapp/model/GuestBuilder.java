5926edcc147af7a2501aa1ada0df8f5d9f782c87
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2023-11-25 21:18:40 +0100

    Added addMethods for all fields

--- src/main/java/pl/kowalczyk/maciej/java/app/bookingapp/model/GuestBuilder.java
+++ src/main/java/pl/kowalczyk/maciej/java/app/bookingapp/model/GuestBuilder.java
@@ -12,5 +12,33 @@ public class GuestBuilder {
     private Address address;
     private List<Reservation> reservations = new ArrayList<>();
 
+    public GuestBuilder addId(Long id) {
+        this.id = id;
+        return this;
+    }
 
+    public GuestBuilder addName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public GuestBuilder addEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public GuestBuilder addPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+        return this;
+    }
+
+    public GuestBuilder addAddress(Address address) {
+        this.address = address;
+        return this;
+    }
+
+    public GuestBuilder addListOfReservations(List<Reservation> reservations) {
+        this.reservations = reservations;
+        return this;
+    }
 }
