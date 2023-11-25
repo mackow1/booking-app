026cc20b5f7ffa862f1af8b4ef3e732beecbd3e7
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2023-11-25 21:08:52 +0100

    Added fields from Guest class

--- src/main/java/pl/kowalczyk/maciej/java/app/bookingapp/model/GuestBuilder.java
+++ src/main/java/pl/kowalczyk/maciej/java/app/bookingapp/model/GuestBuilder.java
@@ -1,4 +1,16 @@
 package pl.kowalczyk.maciej.java.app.bookingapp.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GuestBuilder {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private Address address;
+    private List<Reservation> reservations = new ArrayList<>();
+
+
 }
