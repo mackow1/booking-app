acf57fa7cdd7ec33714aa1c88975470a688fe7c8
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2023-11-28 14:37:40 +0100

    Todo & homework

--- src/main/java/pl/kowalczyk/maciej/java/app/bookingapp/dao/utils/DatabaseConnection.java
+++ src/main/java/pl/kowalczyk/maciej/java/app/bookingapp/dao/utils/DatabaseConnection.java
@@ -30,3 +30,61 @@ public class DatabaseConnection {
 
 // TODO: 28.11.2023
 // Dodać loggery
+
+// TODO: 28.11.2023 PD
+// Stworzyć oddzielną klasse pobierającą namiary na bazę danych/credentials z pliku properties
+// https://docs.oracle.com/javase/tutorial/essential/environment/properties.html
+
+// TODO: 28.11.2023 PD
+// Przećwiczyć left join i right join i przygotować przykłady dla joinów
+
+/*
+-- public class Test{
+--    private int id;
+--    private String name;
+-- }
+CREATE TABLE TEST(
+    ID INT PRIMARY KEY,
+    NAME VARCHAR(255)
+);
+
+-- public class Address {
+--     private int id;
+--     private String street;
+-- }
+
+CREATE TABLE ADDRESSES (
+    ID INT PRIMARY KEY,
+    NAME VARCHAR(255)
+);
+
+-- public class Guest {
+--     private int id;
+--     private String name;
+--     private Address address;
+-- }
+
+CREATE TABLE GUESTS (
+    ID INT PRIMARY KEY,
+    NAME VARCHAR(255),
+    ADDRESS_ID INT,
+    foreign key (ADDRESS_ID) references ADDRESSES(ID)
+);
+
+INSERT INTO ADDRESSES VALUES(1, 'Warszawska 1');
+INSERT INTO ADDRESSES(ID, NAME) VALUES(2, 'Warszawska 6');
+
+SELECT * FROM ADDRESSES ;
+
+INSERT INTO GUESTS( ID, NAME , ADDRESS_ID ) VALUES (1, 'Maciek', null);
+INSERT INTO GUESTS( ID, NAME , ADDRESS_ID ) VALUES (2, 'Piotr', 2);
+
+SELECT * FROM GUESTS ;
+-- Łączenie dwóch tabel za pomocą FK i PK
+SELECT *
+FROM GUESTS AS G
+JOIN ADDRESSES AS A
+ON G.ADDRESS_ID = A.ID;
+
+ */
+
