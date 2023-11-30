82a829bc86436b1cd0e937673f664d1ce48c2bbf
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2023-11-30 16:12:09 +0100

    Added Logger and method

--- src/main/java/pl/kowalczyk/maciej/java/app/bookingapp/dao/utils/DatabaseCredentials.java
+++ src/main/java/pl/kowalczyk/maciej/java/app/bookingapp/dao/utils/DatabaseCredentials.java
@@ -1,4 +1,16 @@
 package pl.kowalczyk.maciej.java.app.bookingapp.dao.utils;
 
+import java.util.logging.Logger;
+
 public class DatabaseCredentials {
+
+    private static final Logger LOGGER = Logger.getLogger(DatabaseCredentials.class.getName());
+    private String filePath = "src/main/resources/database.properties";
+
+    public static String getUrl() {
+        LOGGER.info("getUrl()");
+        String url = "";
+        LOGGER.info("getUrl(...) = " + url);
+        return url;
+    }
 }
