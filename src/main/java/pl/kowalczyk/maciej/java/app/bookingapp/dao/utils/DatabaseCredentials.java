7521f58360cb10713357524f355e0b5535334edc
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2023-11-30 16:49:13 +0100

    Modify method and added File field

--- src/main/java/pl/kowalczyk/maciej/java/app/bookingapp/dao/utils/DatabaseCredentials.java
+++ src/main/java/pl/kowalczyk/maciej/java/app/bookingapp/dao/utils/DatabaseCredentials.java
@@ -1,15 +1,28 @@
 package pl.kowalczyk.maciej.java.app.bookingapp.dao.utils;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class DatabaseCredentials {
 
     private static final Logger LOGGER = Logger.getLogger(DatabaseCredentials.class.getName());
-    private String filePath = "src/main/resources/database.properties";
+    private static final File file = new File("src/main/resources/database.properties");
+
 
     public static String getUrl() {
         LOGGER.info("getUrl()");
         String url = "";
+        Properties properties = new Properties();
+        try (FileInputStream in = new FileInputStream(file)) {
+            properties.load(in);
+            url = properties.getProperty("url");
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, "", e);
+        }
         LOGGER.info("getUrl(...) = " + url);
         return url;
     }
