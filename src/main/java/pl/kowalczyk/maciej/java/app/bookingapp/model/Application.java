80088549a9a02d83ced7bb0f4b4ddd81e79d6edf
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2023-12-01 14:12:10 +0100

    Add Design Patterns Project

--- src/main/java/pl/kowalczyk/maciej/java/app/bookingapp/model/Application.java
+++ src/main/java/pl/kowalczyk/maciej/java/app/bookingapp/model/Application.java
@@ -7,3 +7,17 @@ public class Application {
         konwalioweZacisze.rent();
     }
 }
+
+// TODO: 01.12.2023 PD
+// GANG OF FOUR WZORCE PROJEKTOWE CBS CREATIONAL BEHAVIOURAL STRUCTURAL
+// Projekt restauracji
+// Singleton - DatabaseConnection, RestaurantMenu
+// Prototype - LoyaltyPointPrototype, PaymentFeesPrototype
+// Builder - OrderBuilder, MenuBuilder
+// Facade - RestaurantClientFacade, CheckOutFaced
+// Adapter* - MobileClient, LocalClient
+// Proxy* - PaymentCheckProxy, LoyaltyPointCheckProxy
+// Strategy - PaymentMethodStrategy, OrderMethodStrategy
+// Iterator - ManyItemIterator, OrderIterator
+// Visitor* - OrderCheckOut, LoyaltyPoint
+//
