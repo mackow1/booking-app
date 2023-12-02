3b01bfcf33d06960e6e841a0aa93ab5ae275b236
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2023-12-03 00:39:18 +0100

    Delete old tests

--- src/test/java/pl/kowalczyk/maciej/java/app/bookingapp/dao/utils/DatabaseCredentialsTest.java
+++ src/test/java/pl/kowalczyk/maciej/java/app/bookingapp/dao/utils/DatabaseCredentialsTest.java
@@ -8,36 +8,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class DatabaseCredentialsTest {
 
     @Test
-    void whenGetUrl_thenUrlNotNull() {
-        // given
-        // when
-        String url = DatabaseCredentials.getUrl();
-
-        // then
-        Assertions.assertNotNull(url, "URL is null");
-    }
-
-    @Test
-    void whenGetUser_thenUserNotNull() {
-        // given
-        // when
-        String user = DatabaseCredentials.getUser();
-
-        // then
-        Assertions.assertNotNull(user, "User is null");
-    }
-
-    @Test
-    void whenGetPassword_thenPasswordNotNull() {
-        // given
-        // when
-        String password = DatabaseCredentials.getPassword();
-
-        // then
-        Assertions.assertNotNull(password, "Password is null");
-    }
-
-    @Test
     void givenKeyUrl_whenGetProperty_thenValueNotNull() {
         // given
         String key = "url";
