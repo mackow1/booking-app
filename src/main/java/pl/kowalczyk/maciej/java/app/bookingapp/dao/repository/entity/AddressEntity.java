003987b272527e73907c5b637f965c2d8a2fa238
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2023-12-06 14:37:27 +0100

    Modified fields

--- src/main/java/pl/kowalczyk/maciej/java/app/bookingapp/dao/repository/entity/AddressEntity.java
+++ src/main/java/pl/kowalczyk/maciej/java/app/bookingapp/dao/repository/entity/AddressEntity.java
@@ -1,6 +1,5 @@
 package pl.kowalczyk.maciej.java.app.bookingapp.dao.repository.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -10,25 +9,12 @@ public class AddressEntity {
 
     @Id
     @GeneratedValue
-    @Column(name = "id")
     private Long id;
-
-    @Column(name = "streetName")
     private String street;
-
-    @Column(name = "flatNumber")
     private String flatNumber;
-
-    @Column(name = "cityName")
     private String city;
-
-    @Column(name = "postalCode")
     private String postalCode;
-
-    @Column(name = "voivodeship")
     private String voivodeship;
-
-    @Column(name = "countryName")
     private String country;
 
     public AddressEntity() {
