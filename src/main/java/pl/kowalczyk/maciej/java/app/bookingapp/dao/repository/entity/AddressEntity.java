473533fd36658a5f12bbf1876d1b774474941ec6
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2023-12-06 14:21:01 +0100

    Added fields as columns to table

--- src/main/java/pl/kowalczyk/maciej/java/app/bookingapp/dao/repository/entity/AddressEntity.java
+++ src/main/java/pl/kowalczyk/maciej/java/app/bookingapp/dao/repository/entity/AddressEntity.java
@@ -1,5 +1,6 @@
 package pl.kowalczyk.maciej.java.app.bookingapp.dao.repository.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -9,17 +10,28 @@ public class AddressEntity {
 
     @Id
     @GeneratedValue
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "streetName")
+    private String street;
+
+    @Column(name = "flatNumber")
+    private String flatNumber;
+
+    @Column(name = "cityName")
+    private String city;
+
+    @Column(name = "postalCode")
+    private String postalCode;
+
+    @Column(name = "voivodeship")
+    private String voivodeship;
+
+    @Column(name = "countryName")
+    private String country;
 
     public AddressEntity() {
     }
-
-    //    private String street;
-//    private String flatNumber;
-//    private String city;
-//    private String postalCode;
-//    private String voivodeship;
-//    private String country;
-
 
 }
