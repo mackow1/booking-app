0fdcc94fca367da10df1866c09bf8158cda46998
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2023-12-07 20:22:04 +0100

    Added new class, test and entity to cfg

---
+++ src/main/java/pl/kowalczyk/maciej/java/app/bookingapp/dao/repository/entity/RentalEntity.java
@@ -0,0 +1,20 @@
+package pl.kowalczyk.maciej.java.app.bookingapp.dao.repository.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import pl.kowalczyk.maciej.java.app.bookingapp.model.Guest;
+import pl.kowalczyk.maciej.java.app.bookingapp.model.Reservation;
+
+@Entity
+public class RentalEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private Guest guest;
+    private Reservation reservation;
+
+    public RentalEntity() {
+    }
+}
