1f3ad2543cc0dc93bd3d214174f53d358f644b26
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2023-12-13 23:24:40 +0100

    Added getters and setters, relations

--- src/main/java/pl/kowalczyk/maciej/java/app/bookingapp/dao/repository/entity/PropertyEntity.java
+++ src/main/java/pl/kowalczyk/maciej/java/app/bookingapp/dao/repository/entity/PropertyEntity.java
@@ -3,6 +3,8 @@ package pl.kowalczyk.maciej.java.app.bookingapp.dao.repository.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import pl.kowalczyk.maciej.java.app.bookingapp.model.Address;
 import pl.kowalczyk.maciej.java.app.bookingapp.model.Host;
 import pl.kowalczyk.maciej.java.app.bookingapp.model.Rental;
@@ -12,14 +14,64 @@ import java.util.List;
 @Entity
 public class PropertyEntity {
 
+    // TODO: 13.12.2023 Dodać relację do addresu jak w classie GuestEntity + test 
+
+    // TODO: 13.12.2023 Dodać relację do rentals jak w klasie GuestEntity 
+
     @Id
     @GeneratedValue
     private Long id;
     private String name;
-//    private Address address;
-//    private Host host;
-//    private List<Rental> rentals;
+
+    @OneToOne
+    private AddressEntity address;
+
+    @OneToOne
+    private HostEntity host;
+
+    @OneToMany
+    private List<RentalEntity> rentals;
 
     public PropertyEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public AddressEntity getAddress() {
+        return address;
+    }
+
+    public void setAddress(AddressEntity address) {
+        this.address = address;
+    }
+
+    public HostEntity getHost() {
+        return host;
+    }
+
+    public void setHost(HostEntity host) {
+        this.host = host;
+    }
+
+    public List<RentalEntity> getRentals() {
+        return rentals;
+    }
+
+    public void setRentals(List<RentalEntity> rentals) {
+        this.rentals = rentals;
     }
 }
