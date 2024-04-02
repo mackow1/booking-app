485b3ec391b89143c7bc87714f8ac90bd81c7126
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2024-04-02 13:31:52 +0200

    Added todo

--- src/main/java/pl/kowalczyk/maciej/java/app/bookingapp/BookingApplication.java
+++ src/main/java/pl/kowalczyk/maciej/java/app/bookingapp/BookingApplication.java
@@ -12,4 +12,6 @@ public class BookingApplication {
 
 // TODO: 28.03.2024 Dokończyć implementację brancha dao-refactor-interface (commit-push=pull request)
 // TODO: 28.03.2024 Będąc na branchu adding-service-layer zrobić ręczny merge z branchem main
-// TODO: 28.03.2024 Dokończyć implementację serviców 
+// TODO: 28.03.2024 Dokończyć implementację serviców
+
+// TODO: 02.04.2024 https://bootsnipp.com/snippets/PEZZ7 - kalendarz
