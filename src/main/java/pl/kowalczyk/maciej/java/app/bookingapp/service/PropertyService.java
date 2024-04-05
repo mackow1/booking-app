10739e2293c706138634c47ebb8b3a6d7e03ad3e
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2024-04-05 12:47:06 +0200

    Added method read

--- src/main/java/pl/kowalczyk/maciej/java/app/bookingapp/service/PropertyService.java
+++ src/main/java/pl/kowalczyk/maciej/java/app/bookingapp/service/PropertyService.java
@@ -3,12 +3,14 @@ package pl.kowalczyk.maciej.java.app.bookingapp.service;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 import pl.kowalczyk.maciej.java.app.bookingapp.api.exception.property.PropertyCreateException;
+import pl.kowalczyk.maciej.java.app.bookingapp.api.exception.property.PropertyReadException;
 import pl.kowalczyk.maciej.java.app.bookingapp.dao.repository.PropertyRepository;
 import pl.kowalczyk.maciej.java.app.bookingapp.dao.repository.entity.PropertyEntity;
 import pl.kowalczyk.maciej.java.app.bookingapp.model.Property;
 import pl.kowalczyk.maciej.java.app.bookingapp.service.mapper.PropertyMapper;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -53,5 +55,24 @@ public class PropertyService {
             LOGGER.log(Level.SEVERE, "Database access error while saving property: " + property, e);
             throw new PropertyCreateException("Database access error while saving property:" + property);
         }
+    }
+
+    public Property read(Long id) throws PropertyReadException {
+        LOGGER.info("read(" + id + ")");
+
+        if (id == null) {
+            throw new PropertyReadException("ID must not be NULL");
+        }
+
+        Optional<PropertyEntity> optionalPropertyEntity = propertyRepository.findById(id);
+//        optionalPropertyEntity.orElseThrow(() -> {
+//                    LOGGER.log(Level.SEVERE, "Property not found given id");
+//                }
+//        );
+
+        Property result = null;
+
+        LOGGER.info("read(...) = " + result);
+        return result;
     }
 }
