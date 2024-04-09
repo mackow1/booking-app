44f5a176e6e96f5079b9bbaeec63e11088ec4d79
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2024-04-09 13:55:08 +0200

    Modified dashboard, small fixes

--- src/main/java/pl/kowalczyk/maciej/java/app/bookingapp/model/Reservation.java
+++ src/main/java/pl/kowalczyk/maciej/java/app/bookingapp/model/Reservation.java
@@ -6,9 +6,36 @@ public class Reservation {
     private String checkOut;
     private int numberOfPersons;
 
+    public Reservation() {
+    }
+
     public Reservation(String checkIn, String checkOut, int numberOfPersons) {
         this.checkIn = checkIn;
         this.checkOut = checkOut;
+        this.numberOfPersons = numberOfPersons;
+    }
+
+    public String getCheckIn() {
+        return checkIn;
+    }
+
+    public void setCheckIn(String checkIn) {
+        this.checkIn = checkIn;
+    }
+
+    public String getCheckOut() {
+        return checkOut;
+    }
+
+    public void setCheckOut(String checkOut) {
+        this.checkOut = checkOut;
+    }
+
+    public int getNumberOfPersons() {
+        return numberOfPersons;
+    }
+
+    public void setNumberOfPersons(int numberOfPersons) {
         this.numberOfPersons = numberOfPersons;
     }
 
