ff61e6a2c86323a4f43cc725e8206e3ceef22ec3
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2024-05-04 14:21:49 +0200

    Added config for spring security

---
+++ src/main/java/pl/kowalczyk/maciej/java/app/bookingapp/config/web/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package pl.kowalczyk.maciej.java.app.bookingapp.config.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests((requests) -> requests
+//                                .requestMatchers("/authors").permitAll()
+//                                .requestMatchers("/apartments/create").permitAll()
+                                .requestMatchers("/rentals").hasAnyRole("ADMIN")
+//                                .requestMatchers("/users").permitAll()
+//                                .requestMatchers("/users/create").permitAll()
+                                .requestMatchers("/").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form.permitAll())
+                .logout((logout) -> logout.permitAll());
+
+        return http.build();
+    }
+
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+}
