17b5e725a7e2d85dc93c4235ca3c47b62a1fdfdf
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2024-05-10 13:21:22 +0200

    Added user login classes

--- src/main/java/pl/kowalczyk/maciej/java/app/bookingapp/config/web/BookingAppUserDetailService.java
+++ src/main/java/pl/kowalczyk/maciej/java/app/bookingapp/config/web/BookingAppUserDetailService.java
@@ -1,9 +1,13 @@
 package pl.kowalczyk.maciej.java.app.bookingapp.config.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import pl.kowalczyk.maciej.java.app.bookingapp.dao.repository.UserRepository;
+import pl.kowalczyk.maciej.java.app.bookingapp.dao.repository.entity.UserEntity;
 
 import java.util.logging.Logger;
 
@@ -12,9 +16,25 @@ public class BookingAppUserDetailService implements UserDetailsService {
 
     private static final Logger LOGGER = Logger.getLogger(BookingAppUserDetailService.class.getName());
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         LOGGER.info("loadUserByUsername(" + username + ")");
-        return null;
+
+        UserEntity userEntity = userRepository.findByUsername(username);
+
+        if (userEntity == null) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+
+        UserDetails userDetails = User.withUsername(userEntity.getUsername())
+                .password(userEntity.getPassword())
+                .roles("USER")
+                .build();
+
+        LOGGER.info("loadUserByUsername(...) = " + userDetails);
+        return userDetails;
     }
 }
---
+++ src/main/java/pl/kowalczyk/maciej/java/app/bookingapp/dao/repository/entity/UserEntity.java
@@ -0,0 +1,54 @@
+package pl.kowalczyk.maciej.java.app.bookingapp.dao.repository.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "USERS")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String username;
+    private String password;
+
+    public UserEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "UserEntity{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
