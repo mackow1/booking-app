f26eecc6dcdc29b2fcfc548bc149363e7ee6cf75
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2024-09-05 14:37:45 +0200

    Added new test class

---
+++ src/test/java/pl/kowalczyk/maciej/java/app/bookingapp/dao/repository/UserRepositorySpringTest.java
@@ -0,0 +1,21 @@
+package pl.kowalczyk.maciej.java.app.bookingapp.dao.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserRepositorySpringTest {
+
+    @Test
+    void findByUsername() {
+        // given
+
+        // when
+
+        // then
+
+    }
+
+}
