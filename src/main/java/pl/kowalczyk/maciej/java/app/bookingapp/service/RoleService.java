1782279764835cfb28e0cc11cfa470f02369bb26
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2024-09-10 20:24:37 +0200

    Added new method and test

--- src/main/java/pl/kowalczyk/maciej/java/app/bookingapp/service/RoleService.java
+++ src/main/java/pl/kowalczyk/maciej/java/app/bookingapp/service/RoleService.java
@@ -1,12 +1,15 @@
 package pl.kowalczyk.maciej.java.app.bookingapp.service;
 
 import org.springframework.stereotype.Service;
+import pl.kowalczyk.maciej.java.app.bookingapp.api.exception.role.RoleReadException;
 import pl.kowalczyk.maciej.java.app.bookingapp.dao.repository.RoleRepository;
 import pl.kowalczyk.maciej.java.app.bookingapp.dao.repository.entity.RoleEntity;
 import pl.kowalczyk.maciej.java.app.bookingapp.model.Role;
 import pl.kowalczyk.maciej.java.app.bookingapp.service.mapper.RoleMapper;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @Service
@@ -30,5 +33,20 @@ public class RoleService {
 
         LOGGER.info("list(...) = " + roles);
         return roles;
+    }
+
+    public Role read(Long id) throws RoleReadException {
+        LOGGER.info("read(" + id + ")");
+
+        Optional<RoleEntity> optionalRoleEntity = roleRepository.findById(id);
+        RoleEntity roleEntity = optionalRoleEntity.orElseThrow(() -> {
+            LOGGER.log(Level.SEVERE, "Role not found for given id: " + id);
+            return new RoleReadException("Role not found for given id: " + id);
+        });
+
+        Role role = roleMapper.from(roleEntity);
+
+        LOGGER.info("read(...) = " + role);
+        return role;
     }
 }
